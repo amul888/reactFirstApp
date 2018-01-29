49bb830fda9a6d59580335138c3b212ab7601a57
Amul <amul888chandra@gmail.com>
2018-01-29 14:24:22 +0530

    fixed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-// import User from './user'
 
 export default class Layout extends Component{
   render(){
     return(
-      <div>hhhhhhhhhhhhh</div>
+      <div>Layout</div>
     )
   }
 }
